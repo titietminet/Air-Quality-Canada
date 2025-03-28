d30eed82da2462ee2c91e04b838a450e06678a0e
titouan <81023724+titietminet@users.noreply.github.com>
2025-03-28 12:39:14 +0100

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ import { Controller } from "../controller/controller.js";
 import { ControllerRepas } from "../controller/controllerRepas.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (window.location.pathname === "/repas.html") {
+    if (window.location.pathname === "/Air-Quality-Canada/repas.html") {
     new ControllerRepas();
     } else {
     new Controller();
